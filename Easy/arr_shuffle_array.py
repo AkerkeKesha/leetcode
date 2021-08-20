e2dadbab1a874732e102a53c4d6f2a7313170897
AkerkeKesha <akerke.kass@gmail.com>
2021-08-20 22:03:09 +0200

    created and fixed tests

--- Easy/arr_shuffle_array.py
+++ Easy/arr_shuffle_array.py
@@ -50,5 +50,8 @@ if __name__ == '__main__':
         ([2,5,1,3,4,7], 3, [2,3,5,4,1,7]),
         ([1,2,3,4,4,3,2,1], 4, [1,4,2,3,3,2,4,1]),
         ([1,1,2,2], 2, [1,2,1,2]),
-        [], 0, []
+        ([], 0, [])
     ]
+    for nums, n, expected in tests:
+        result = Solution().shuffle(nums, n)
+        assert expected == result
