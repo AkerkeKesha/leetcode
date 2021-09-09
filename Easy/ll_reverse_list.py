624e183ed57620336eee6f9ac924a3a416c59c81
AkerkeKesha <akerke.kass@gmail.com>
2021-09-09 22:09:22 +0200

    changed testcase from return None, to return empty list

--- Easy/ll_reverse_list.py
+++ Easy/ll_reverse_list.py
@@ -58,8 +58,8 @@ class Solution:
 
 if __name__ == '__main__':
     tests = [
-        ([], None),
-        (None, None),
+        ([], []),
+        (None, []),
         ([1, 2], [2, 1]),
         ([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]),
     ]
