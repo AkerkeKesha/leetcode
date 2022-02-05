51dc003b5615ca26c981e4786f4f5e0a17c1802f
AkerkeKesha <akerke.kass@gmail.com>
2022-02-05 21:25:17 +0100

    refactored for more pythonic style

--- Easy/greedy_flowerbed.py
+++ Easy/greedy_flowerbed.py
@@ -35,8 +35,7 @@ class Solution:
 
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         count_flowers = 0
-        i = 0
-        while i < len(flowerbed):
+        for i in range(len(flowerbed)):
             if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i+1] == 0):
                 flowerbed[i] = 1
                 count_flowers += 1
