65a669d2442b81e1597874f06c7fe7bc71a661c3
AkerkeKesha <akerke.kass@gmail.com>
2022-10-11 14:22:29 +0200

    implemented quick union class as union find implementatio (more efficitient one)

--- basics/graphs.py
+++ basics/graphs.py
@@ -39,6 +39,44 @@ class QuickFind:
         return self.find(x) == self.find(y)
 
 
+class UnionFind:
+
+    def __init__(self, size: int) -> None:
+        self.root = [i for i in range(size)]
+
+    def find(self, x) -> int:
+        """
+        O(N) to make changes and root index
+        :param x:
+        :return:
+        """
+        while x != self.root[x]:
+            x = self.root[x]
+        return self.root[x]
+
+    def union(self, x: int, y: int) -> None:
+        """
+        Make the root of y be the root of x
+        O(N) to make changes
+        :param x:
+        :param y:
+        :return:
+        """
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            self.root[rootY] = rootX
+
+    def connected(self, x: int, y: int) -> bool:
+        """
+        O(N) to check whether root vertices are equal
+        :param x:
+        :param y:
+        :return:
+        """
+        return self.find(x) == self.find(y)
+
+
 if __name__ == '__main__':
     qf = QuickFind(10)
     # 1-2-5-6-7 3-8-9 4
@@ -57,4 +95,20 @@ if __name__ == '__main__':
     # 1-2-5-6-7 3-8-9-4
     qf.union(9, 4)
     assert(qf.connected(4, 9))  # true
+
+    # Test Case
+    uf = UnionFind(10)
+    # 1-2-5-6-7 3-8-9 4
+    uf.union(1, 2)
+    uf.union(2, 5)
+    uf.union(5, 6)
+    uf.union(6, 7)
+    uf.union(3, 8)
+    uf.union(8, 9)
+    assert(uf.connected(1, 5))  # true
+    assert(uf.connected(5, 7))  # true
+    assert(not uf.connected(4, 9))  # false
+    # 1-2-5-6-7 3-8-9-4
+    uf.union(9, 4)
+    assert(uf.connected(4, 9))  # true
 
